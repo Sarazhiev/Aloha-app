29be389736ff5d4b6cd7f12f390cafa58ef39ead
Sarazhiev <dilmanurs@mail.ru>
2022-07-17 21:41:09 +0600

    fixed bubs with adaptation the all sizes and pushed to the host real server and so on

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
@@ -17,4 +16,3 @@ root.render(
 
 );
 
-reportWebVitals();
